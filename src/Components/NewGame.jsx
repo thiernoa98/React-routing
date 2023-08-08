3eea88f0bd5368f88fa8065eb82e2ac56e6d27f0
Thierno Barry <117252418+thiernoa98@users.noreply.github.com>
2023-08-08 00:59:17 -0400

    Add files via upload

---
+++ src/Components/NewGame.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NewGame() {
+  return (
+    <div>
+      <form action="">
+        <label>
+            This is where you would create a new game:
+            <input type='text'  name="newGame"/>
+        </label>
+      </form>
+    </div>
+  )
+}
+
+export default NewGame
