f5db026f26d0c35983072a3ec79065b7f13c5ea7
Thierno Barry <117252418+thiernoa98@users.noreply.github.com>
2023-08-08 00:59:33 -0400

    Add files via upload

---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+      <h1 style={{color: 'red'}}>Error: 404 Page Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFound
